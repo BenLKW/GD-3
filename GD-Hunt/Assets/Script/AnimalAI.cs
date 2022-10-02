da87adc38eb52326596cbe00f3ff303e891a1b7b
SamLu000 <samluxian@gmail.com>
2022-10-02 17:03:16 +0800

    no message

--- GD-Hunt/Assets/Script/AnimalAI.cs
+++ GD-Hunt/Assets/Script/AnimalAI.cs
@@ -8,8 +8,8 @@ public class AnimalAI : MonoBehaviour
     public NavMeshAgent animal;
     public Transform player;
     public float lookRadius = 10f;
-    
-    
+    public float range;
+    public Transform centrePoint;
 
     private void Awake()
     {
@@ -21,7 +21,50 @@ public class AnimalAI : MonoBehaviour
     void Update()
     {
 
+        RunAwayFromPlayer();
 
+
+        if (animal.remainingDistance <= animal.stoppingDistance) 
+        {
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, range, out point)) 
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); 
+                animal.SetDestination(point);
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; 
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) 
+        {
+            
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+    private void RunAwayFromPlayer()
+    {
         float distance = Vector3.Distance(player.position, transform.position);
 
         Debug.Log("Animal Distance:" + distance);
@@ -33,19 +76,7 @@ public class AnimalAI : MonoBehaviour
 
             animal.SetDestination(newPos);
         }
-
-        
-
-
     }
 
-    
-   
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
-    }
-    
 }
