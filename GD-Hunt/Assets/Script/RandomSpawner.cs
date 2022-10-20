087e785077db0de977995fa543d7a36464f2f770
SamLu000 <samluxian@gmail.com>
2022-10-20 23:20:15 +0800

    no message

--- GD-Hunt/Assets/Script/RandomSpawner.cs
+++ GD-Hunt/Assets/Script/RandomSpawner.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class RandomSpawner : MonoBehaviour
 {
     public GameObject SpawnObject;
+    public Transform spawnpoint;
     public int xPos;
     public int zPos;
     public int enemyCount;
+    
+
+    
    
 
     void Start()
     {
         StartCoroutine (EnemySpawn());
-
+        
     }
 
     IEnumerator EnemySpawn()
     {
         while (enemyCount<10)
         {
-            xPos = Random.Range(-20,20);
-            zPos = Random.Range(-20, 25);
-            Instantiate(SpawnObject, new Vector3(xPos, 0, zPos), Quaternion.identity);
+            Instantiate(SpawnObject, spawnpoint.position, Quaternion.identity);
             yield return new WaitForSeconds(0.1f);
             enemyCount += 1;
 
