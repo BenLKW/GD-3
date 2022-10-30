00e354573774da0ff2f0a506a1739d3a7d926077
SamLu000 <samluxian@gmail.com>
2022-10-30 12:07:55 +0800

    no message

---
+++ GD-Hunt/Assets/Script/EnemyHealth.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public  float health;
+    public float maxHealth;
+
+    public GameObject healthBar;
+    public Slider slider;
+    
+
+    private void Start()
+    {
+        health = maxHealth;
+        slider.value = CalculateHealth();
+        
+    }
+
+    private void Update()
+    {
+        
+        slider.value = CalculateHealth();
+        slider.transform.LookAt(Camera.main.transform.position);
+        slider.transform.Rotate(0, 180, 0);
+        healthBar.transform.LookAt(Camera.main.transform.position);
+        healthBar.transform.Rotate(0, 180, 0);
+
+        if (health < maxHealth)
+        {
+            healthBar.SetActive(true);
+        }
+        
+        
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        HealthDetect();
+
+    }
+    public void HealthDetect()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            health--;
+        }
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private float CalculateHealth()
+    {
+        return health / maxHealth;
+    }
+
+    
+       
+
+    
+}
