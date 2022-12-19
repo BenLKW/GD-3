18858a05e63030d3874d33936d6d6a5456b34184
SamLu000 <samluxian@gmail.com>
2022-12-19 10:33:29 +0800

    no message

--- GD-Hunt/Assets/Script/BossAI.cs
+++ GD-Hunt/Assets/Script/BossAI.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossAI : MonoBehaviour
 {
+    public Animator anim;
+
+    private NavMeshAgent boss;
+
+    public float range;
+    public Transform BossPatrollingCenter;
+
+    public LayerMask whatIsPlayer;
+
+    public float lookRadius = 10f;
+    public bool playerInLookRadius;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        boss = GetComponent<NavMeshAgent>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +30,43 @@ public class BossAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerInLookRadius = Physics.CheckSphere(transform.position, lookRadius, whatIsPlayer);
+        anim.SetFloat("Speed", boss.velocity.magnitude);
+        if (!playerInLookRadius)
+        {
+            Patroling();
+        }
         
     }
+    private void Patroling()
+    {
+
+        if (boss.remainingDistance <= boss.stoppingDistance)
+        {
+            Vector3 point;
+            if (RandomPoint(BossPatrollingCenter.position, range, out point))
+            {
+
+                boss.SetDestination(point);
+
+            }
+        }
+    }
+
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+
 }
